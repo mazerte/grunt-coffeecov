6b1dd2c9253789691e930a550b03808b57b4b334
Paul Roebuck <plroebuck@users.noreply.github.com>
2018-03-13 07:31:28 -0500

    test(test-coffeecov.js): Fix tests for non-default options.path values
    Added logic to replicate Mocha's instrumented file naming scheme.
    
    Fixes: #7

--- test/test-coffeecov.js
+++ test/test-coffeecov.js
@@ -2,8 +2,9 @@ var chai = require('chai')
   , expect = chai.expect
   , should = chai.should()
   , chaiFS = require('chai-fs')
+  , fs = require('fs')
   , grunt = require('grunt')
-  , fs = require('fs');
+  , path = require('path');
 
 chai.use(chaiFS);
 
@@ -13,21 +14,68 @@ function checkSrc() {
   expect("test/src/exclude/file.coffee").to.be.a.file().and.not.empty;
 }
 
-function checkCov(variable, exclude) {
-  if(!variable) {
-    variable = "_$jscoverage";
+/**
+ * @param {string} givenPath - Pathname of file being instrumented
+ * @param {string} [pathopt=none] - Value of 'options.path' setting
+ */
+function instrumentedPath(givenPath, pathopt) {
+  var evaluatedPath = path.normalize(givenPath);
+  if (path.isAbsolute(evaluatedPath)) {
+    // Convert absolute pathname to root-relative form
+    evaluatedPath = evaluatedPath.slice(1);
   }
 
-  expect("test/src-cov/test.js").to.be.a.file().and.not.empty;
-  content = fs.readFileSync('test/src-cov/test.js', 'utf8');
-  content.should.have.string(variable + '["test.coffee"]');
+  if (!pathopt) {
+    pathopt = 'none';
+  }
 
-  if(exclude) {
-    expect("test/src-cov/exclude.js").to.not.be.a.path();
-    expect("test/src-cov/exclude/file.js").to.not.be.a.path();
-  } else {
-    expect("test/src-cov/exclude.js").to.be.a.file().and.not.empty;
-    expect("test/src-cov/exclude/file.js").to.be.a.file().and.not.empty;
+  switch (pathopt) {
+    case 'abbr':
+      var pathcomps = [];
+      path.dirname(evaluatedPath).split('/').forEach(function (dir) {
+        pathcomps.push(dir.charAt(0));
+      });
+      pathcomps.push(path.basename(evaluatedPath));
+      return pathcomps.join('/');
+      break;
+    case 'relative':
+      return evaluatedPath;
+      break;
+    default:
+      return path.basename(evaluatedPath);
+  }
+}
+
+/**
+ * @param {string} [variable=_$jscoverage] - Value of 'options.coverageVar' setting
+ * @param {boolean} [exclude=false] - Did the instrumentation exclude files?
+ * @param {string} [pathopt=none] - Value of 'options.path' setting
+ */
+function checkCov(variable, exclude, pathopt) {
+  if (!variable) {
+    variable = '_$jscoverage';
+  }
+
+  if (!pathopt) {
+    pathopt = 'none';
+  }
+
+  var sourceFile = 'test/src/test.coffee';
+  var transpiledFile = 'test/src-cov/test.js';
+
+  expect(transpiledFile).to.be.a.file().and.not.empty;
+  content = fs.readFileSync(transpiledFile, 'utf8');
+
+  var instPath = instrumentedPath(sourceFile, pathopt);
+  content.should.have.string(variable + '["' + instPath + '"]');
+
+  if (exclude) {
+    expect('test/src-cov/exclude.js').to.not.be.a.path();
+    expect('test/src-cov/exclude/file.js').to.not.be.a.path();
+  }
+  else {
+    expect('test/src-cov/exclude.js').to.be.a.file().and.not.empty;
+    expect('test/src-cov/exclude/file.js').to.be.a.file().and.not.empty;
   }
 }
 
@@ -71,24 +119,29 @@ describe("Grunt CoffeeCov", function() {
 
   describe('when given alternate options to show path in instrumented output', function () {
 
+    var variable = '_$jscoverage';
+    var sourceDir = 'test/src/';
+    var sourceFile = sourceDir + 'exclude/file.coffee';
+    var transpiledFile = 'test/src-cov/exclude/file.js';
+
     it('should replace each directory with its first letter given "abbr"', function(done) {
       var child = grunt.util.spawn({ grunt: true, args: ['coffeecov:abbr'] }, function() {
-        checkCov();
-
-        content = fs.readFileSync('test/src-cov/exclude/file.js', 'utf8');
-        content.should.have.string('_$jscoverage["exclude/file.coffee"]');
-
+        var pathopt = 'abbr';
+        checkCov(undefined, false, pathopt);
+        content = fs.readFileSync(transpiledFile, 'utf8');
+        var instPath = instrumentedPath(sourceFile, pathopt);
+        content.should.have.string(variable + '["' + instPath + '"]');
         done();
       });
     });
 
     it('should use the file\'s relative path given "relative"', function(done) {
       var child = grunt.util.spawn({ grunt: true, args: ['coffeecov:relative'] }, function() {
-        checkCov();
-
-        content = fs.readFileSync('test/src-cov/exclude/file.js', 'utf8');
-        content.should.have.string('_$jscoverage["exclude/file.coffee"]');
-
+        var pathopt = 'relative';
+        checkCov(undefined, false, pathopt);
+        content = fs.readFileSync(transpiledFile, 'utf8');
+        var instPath = instrumentedPath(sourceFile, pathopt);
+        content.should.have.string(variable + '["' + instPath + '"]');
         done();
       });
     });
